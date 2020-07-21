102e6e3a36b7da4cbf5bcbb09ea1a9a678bb9834
FreakJazz <jazzrb2307@hotmail.com>
2020-07-21 17:17:26 -0500

    Put indications in the program and configure pins

--- program/program.ino
+++ program/program.ino
@@ -7,23 +7,23 @@ Separador s;
 #define EEPROM_SIZE 3
 
 // Define stepper motor 1 connections
-#define PUL1 34 //define Pulse pin
-#define DIR1 39 //define Direction pin
+#define PUL1 35 //define Pulse pin
+#define DIR1 34 //define Direction pin
 
 // Define stepper motor 2 connections
 #define PUL2 33 //define Pulse pin
 #define DIR2 32 //define Direction pin
 
 // Define stepper motor 3 connections
-#define PUL3 27 //define Pulse pin
-#define DIR3 26 //define Direction pin
+#define PUL3 26 //define Pulse pin
+#define DIR3 25 //define Direction pin
 
 
 // Declarate the position of the motors
 int positions [EEPROM_SIZE]= {};   
 int PUL;  //define Pulse pin
 int DIR;  //define Direction pin
-int incomingByte = 0; // for incoming serial data
+int posi_final, posi_actual;
 
 void setup() {
   
@@ -35,46 +35,38 @@ void setup() {
     // configurate pins of the step motor 1 
     pinMode (PUL1, OUTPUT);
     pinMode (DIR1, OUTPUT);
-
+    digitalWrite(PUL1,LOW);
+    digitalWrite(DIR1,LOW);
+    
     // configurate pins of the step motor 2
     pinMode (PUL2, OUTPUT);
     pinMode (DIR2, OUTPUT);
-
+    digitalWrite(PUL2,LOW);
+    digitalWrite(DIR2,LOW);
+    
     // configurate pins of the step motor 3
     pinMode (PUL3, OUTPUT);
     pinMode (DIR3, OUTPUT);
+    digitalWrite(PUL3,LOW);
+    digitalWrite(DIR3,LOW);
 
     ///////////////// Configure valve 1 //////////////////
     //////////////////////////////////////////////////////
     positions[0] = byte(EEPROM.read(0));
+    Serial.print("Valve 1: ");
     Serial.println(positions[0]); 
-    //give position recive valve, position actual and position final
-    positions[0] = give_position(0, positions[0], 10);
-    //save_valve revice position EEPROM valve and value
-    save_value(0, positions[0]);
-    Serial.println(positions[0]); 
-
+    
     ///////////////// Configure valve 2 //////////////////
     //////////////////////////////////////////////////////
     positions[1] = byte(EEPROM.read(1));
+    Serial.print("Valve 2: "); 
     Serial.println(positions[1]); 
-    //give position recive valve, position actual and position final
-    positions[1] = give_position(1, positions[1], 10);
-    //save_valve revice position EEPROM valve and value
-    save_value(1, positions[1]);
-    Serial.println(positions[1]); 
-
     ///////////////// Configure valve 3 //////////////////
     //////////////////////////////////////////////////////
     positions[2] = byte(EEPROM.read(2));
-    Serial.println(positions[2]); 
-    //give position recive valve, position actual and position final
-    positions[2] = give_position(2, positions[2], 10);
-    //save_valve revice position EEPROM valve and value
-    save_value(2, positions[2]);
+    Serial.print("Valve 3: ");
     Serial.println(positions[2]); 
 
-    EEPROM.commit();
     Serial.println();
     Serial.println("Positions are gotten");
 
@@ -90,26 +82,47 @@ void loop() {
     if (Serial.available()) {
         //Get data to the Serial
         String get_data = Serial.readString();
+        //Separate the data serial 
         String valve = s.separa(get_data,',',0);
         String angle = s.separa(get_data,',',1);
         Serial.println("The valve is: "+ valve);
         Serial.println("The porcent is: "+ angle);
+        // Change the variables to int
         int valve1  = valve.toInt()-1;
         int angle1  = angle.toInt();
-        posi_final = give_position(valve1,positions[valve1],angle1)
+        
+        // function to open and close the valves
+        positions[valve1] = give_position(valve1,positions[valve1],angle1);
+        
         // Save in the EEPROM the last position
-        EEPROM.write(valve1, posi_final);
+        EEPROM.write(valve1, positions[valve1]);
         EEPROM.commit();
-        Serial.println("level saved in flash memory");
-        Serial.println(valve1);
-        Serial.println(angle1);
+        Serial.println("Level saved in memory, this is the actual position");
+        // Show the positions of the valve
+        Serial.print("You change the position of the valve");
+        Serial.println(valve1+1);
+        Serial.println(positions[valve1]);
         delay(500);
+        Serial.println("");
+        // Show the position of the system
+        Serial.println("Now the positions of the valves are: ");
+        Serial.print("Valve 1: ");
+        Serial.println(positions[0]); 
+        Serial.print("Valve 2: ");
+        Serial.println(positions[1]); 
+        Serial.print("Valve 3: ");
+        Serial.println(positions[2]); 
         Serial.println("Please enter the valve and angle of motion separated by a comma");
     } 
 }
 
+// This function choose if the valve needs to open or close
 int give_position(int valve, int posi_actual, int posi_final){
+    delay(100);
+    Serial.println("This is the position function");
     switch(valve){
+    Serial.print("You select the valve:");
+    Serial.print(valve+1); 
     case 0:
         PUL  = PUL1;
         DIR  = DIR1;
@@ -124,46 +137,48 @@ int give_position(int valve, int posi_actual, int posi_final){
         break;
     
     default:
+      Serial.println("This valve doesn't exist");
         break;
     }
 
-    if (posi_actual > posi_final){
+    if (posi_actual >= posi_final){
         //send PUL DIR ENA Position
         posi_final = closing(DIR, PUL, posi_final, posi_actual);
-        Serial.Print("The valve is closing");
+        Serial.println("The valve is closed");
     }
-    if (posi_actual > posi_final){
+    if (posi_actual <= posi_final){
         //send PUL DIR ENA Position
         posi_final = opening(DIR, PUL, posi_final, posi_actual);
-        Serial.Print("The valve is closing");
+        Serial.println("The valve is openned");
     }
     return posi_final;
 }
 
+// This is the open function
 int opening(int DIR, int PUL, int posi_final, int posi_actual){
-     Serial.print("The valve is openning");
+    Serial.println("The valve is openning");
     while (posi_actual < posi_final)
     {
         digitalWrite(DIR,LOW);
         digitalWrite(PUL,HIGH);
-        delayMicroseconds(50);
+        delayMicroseconds(500);
         digitalWrite(PUL,LOW);
-        delayMicroseconds(50);
+        delayMicroseconds(500);
         posi_actual++;
     }
     return posi_actual;
 }
 
-
-int closing(int DIR, int PUL, int posi_actual ,int posi_final, int posi_actual){
-     Serial.print("The valve is closing");
+// This is the close function
+int closing(int DIR, int PUL, int posi_final, int posi_actual){
+     Serial.println("The valve is closing");
      while (posi_actual > posi_final)
     {
         digitalWrite(DIR,HIGH);
         digitalWrite(PUL,HIGH);
-        delayMicroseconds(50);
+        delayMicroseconds(500);
         digitalWrite(PUL,LOW);
-        delayMicroseconds(50);
+        delayMicroseconds(500);
         posi_actual--;
     }
     return posi_actual;
